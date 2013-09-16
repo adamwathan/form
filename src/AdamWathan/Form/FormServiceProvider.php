da8c72ad04704e8f115921cfffd70ea7ffb684fc
Adam Wathan <adam.wathan@gmail.com>
2013-09-15 20:32:46 -0400

    WIP

---
+++ src/AdamWathan/Form/FormServiceProvider.php
@@ -0,0 +1,65 @@
+<?php namespace AdamWathan\Form;
+
+use Illuminate\Support\ServiceProvider;
+use AdamWathan\Form\ErrorStore\IlluminateErrorStore;
+use AdamWathan\Form\OldInput\IlluminateOldInputProvider;
+
+class FormServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->registerErrorStore();
+		$this->registerOldInput();
+		$this->registerFormBuilder();
+	}
+
+	protected function registerErrorStore()
+	{
+		$this->app['adamwathan.form.errorstore'] = $this->app->share(function($app)
+		{
+			return new IlluminateErrorStore($app['session']);
+		});
+	}
+
+	protected function registerOldInput()
+	{
+		$this->app['adamwathan.form.oldinput'] = $this->app->share(function($app)
+		{
+			return new IlluminateOldInputProvider($app['session']);
+		});
+	}
+
+	protected function registerFormBuilder()
+	{
+		$this->app['adamwathan.form'] = $this->app->share(function($app)
+		{
+			$formBuilder = new FormBuilder;
+			$formBuilder->setErrorStore($app['adamwathan.form.errorstore']);
+			$formBuilder->setOldInputProvider($app['adamwathan.form.oldinput']);
+
+			return $formBuilder;
+		});
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array('adamwathan.form');
+	}
+}
