a1384ab91a943cd4e55ffedf8ebfcbe0fb81de20
Adam Wathan <adam.wathan@gmail.com>
2013-09-15 20:20:51 -0400

    WIP

---
+++ src/AdamWathan/Form/ErrorStore/IlluminateErrorStore.php
@@ -0,0 +1,41 @@
+<?php namespace AdamWathan\Form\ErrorStore;
+
+use Illuminate\Session\Store as Session;
+
+class IlluminateErrorStore implements ErrorStoreInterface
+{
+	private $session;
+
+	public function __construct(Session $session)
+	{
+		$this->session = $session;
+	}
+
+	public function hasError($key)
+	{
+		if ( ! $this->hasErrors()) {
+			return false;
+		}
+
+		return $this->getErrors()->has($key);
+	}
+
+	public function getError($key)
+	{
+		if ( ! $this->hasError($key)) {
+			return null;
+		}
+
+		return $this->getErrors()->first($key);	
+	}
+
+	protected function hasErrors()
+	{
+		return $this->session->has('errors');
+	}
+
+	protected function getErrors()
+	{
+		return $this->hasErrors() ? $this->session->get('errors') : null;
+	}
+}
---
+++ src/AdamWathan/Form/OldInput/IlluminateOldInputProvider.php
@@ -0,0 +1,23 @@
+<?php namespace AdamWathan\Form\OldInput;
+
+use Illuminate\Session\Store as Session;
+
+class IlluminateOldInputProvider implements OldInputInterface
+{
+	private $session;
+
+	public function __construct(Session $session)
+	{
+
+	}
+
+	public function hasOldInput($key)
+	{
+		return $this->session->hasOldInput($key);
+	}
+
+	public function getOldInput($key)
+	{
+		return $this->session->getOldInput($key);
+	}
+}
