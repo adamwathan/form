df2674d37412c9de37cb9b3407eb0d2496bb0c35
Adam Wathan <adam.wathan@gmail.com>
2013-12-26 12:29:56 -0500

    Add Laravel 4 facade accessor

---
+++ src/AdamWathan/Form/Facades/Form.php
@@ -0,0 +1,14 @@
+<?php namespace AdamWathan\Form\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Form extends Facade {
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor() { return 'adamwathan.form'; }
+
+}
