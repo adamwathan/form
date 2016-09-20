93524cfd28ac6d1fce0d9c8b71f6ebc13ee6c662
Aydin Hassan <aydin@hotmail.co.uk>
2016-09-20 12:52:04 +0200

    Test for DateTimeLocal DateTime formatting

--- tests/DateTimeLocalTest.php
+++ tests/DateTimeLocalTest.php
@@ -15,4 +15,13 @@ class DateTimeLocalTest extends PHPUnit_Framework_TestCase
     {
         return 'datetime-local';
     }
+
+    public function testDateTimeValuesAreBoundAsFormattedStrings()
+    {
+        $dateTimeLocal = new DateTimeLocal('dob');
+        $dateTimeLocal->value(new DateTime('12-04-1988 10:33'));
+
+        $expected = '<input type="datetime-local" name="dob" value="1988-04-12T10:33">';
+        $this->assertSame($expected, $dateTimeLocal->render());
+    }
 }
